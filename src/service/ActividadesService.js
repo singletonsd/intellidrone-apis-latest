f54eaa921a4b6a92f03e89f74d6c1e940c1628d9
Patricio Perpetua <patricio.perpetua.arg@gmail.com>
2019-01-16 13:16:36 -0300

    Fixed getActividades

--- src/service/ActividadesService.js
+++ src/service/ActividadesService.js
@@ -2,19 +2,20 @@
 
 const streamifier = require('streamifier');
 const es = require('event-stream');
-const moment = require('moment');
 const vacasService = require('./VacasService');
 const vacasModel = require('../database/models/vacas');
 const usersModel = require('../database/models/users');
 const lotesModel = require('../database/models/lotes');
 const actividadesModel = require('../database/models/actividades');
 
+const mongoose = require('../database/main');
+
 const pattern_old = '%{NOTSPACE:primerString} %{NOTSPACE:segundo} \\-- ID: %{WORD:id}, COUNT:%{NUMBER:numero_mensaje},  MSG:%{SPACE:inecesario}%{INT:tiempo_adq_satelite},%{NUMBER:latitud},%{NUMBER:longitud},%{NUMBER:dia},%{NUMBER:hora},%{NUMBER:minuto},%{NUMBER:segundo}';
 const pattern_new = '%{NOTSPACE:primerString} %{NOTSPACE:segundo} \\-- ID: %{WORD:id}, MSG: %{NUMBER:inecesario},%{NUMBER:tiempo_adq_satelite},%{NUMBER:latitud},%{NUMBER:longitud},%{NUMBER:dia},%{NUMBER:hora},%{NUMBER:minuto},%{NUMBER:segundo},%{NUMBER:intentosLora} ';
 const node_grok = require('node-grok').loadDefaultSync();
 const parser_old = node_grok.createPattern(pattern_old);
 const parser_new = node_grok.createPattern(pattern_new);
-const populate = {path: 'vaca', select: 'name reference'};
+const populate = { path: 'vaca', select: 'name reference' };
 
 /**
  * Add one Actividad.
@@ -104,7 +105,7 @@ exports.addActividades = async function (upfile, userId, loteId, collectingDate)
         s.pause();
         line_number++;
         let arreglo = parser_old.parseSync(line);
-        if(!arreglo)
+        if (!arreglo)
           arreglo = parser_new.parseSync(line);
         if (arreglo) {
           // let fechaConstruida = moment(arreglo.primerString);
@@ -113,14 +114,14 @@ exports.addActividades = async function (upfile, userId, loteId, collectingDate)
           // fechaConstruida.add(arreglo.segundo,'seconds');
           let res = arreglo.dia.split("");
           let fechaConstruida = new Date();
-          if(res.length === 5){
+          if (res.length === 5) {
             fechaConstruida.setFullYear("20" + res[3] + res[4]);
             fechaConstruida.setMonth(Number("" + res[1] + res[2]) - 1);
             fechaConstruida.setDate(res[0]);
             fechaConstruida.setHours(arreglo.hora);
             fechaConstruida.setMinutes(arreglo.minuto);
             fechaConstruida.setSeconds(arreglo.segundo);
-          }else{
+          } else {
             fechaConstruida.setFullYear("20" + res[4] + res[5]);
             fechaConstruida.setMonth(Number("" + res[2] + res[3]) - 1);
             fechaConstruida.setDate(Number("" + res[0] + res[1]));
@@ -135,12 +136,12 @@ exports.addActividades = async function (upfile, userId, loteId, collectingDate)
             collectingDate: collectingDate,
             insertDate: new Date()
           });
-          if(arreglo.intentosLora)
+          if (arreglo.intentosLora)
             act.connectionsAttempts = arreglo.intentosLora;
           vacaIndex = vacasRef.indexOf(arreglo.id);
           if (vacaIndex === -1) {
-            if(vacasIdAll.indexOf(arreglo.id) !== -1)
-              reject({ message: 'Vaca with reference ' + arreglo.id + ' exists and it belongs to another user or lote.'});
+            if (vacasIdAll.indexOf(arreglo.id) !== -1)
+              reject({ message: 'Vaca with reference ' + arreglo.id + ' exists and it belongs to another user or lote.' });
             let vaquita = new vacasModel({
               location: loteId,
               reference: arreglo.id
@@ -262,42 +263,84 @@ exports.getActividadById = async function (id, userId) {
  * returns List
  **/
 exports.getActividades = async function (skip, limit, orderBy, filter,
-    userId, loteId, vacaId, fromDate, untilDate,vacaReference) {
-  if(limit <= 0)
+  userId, loteId, vacaId, fromDate, untilDate, vacaReference) {
+  if (limit <= 0)
     limit = 10;
   let vacasId;
   let optionsFind = {};
-  if(vacaId)
-    optionsFind.vaca = vacaId;
-  else if(vacaReference){
-    vacaId = await vacasModel.findOne({ reference: vacaReference});
-    if(vacaId)
-      optionsFind.vaca = vacaId.id;
-  }else{
-    if(userId){
-      let vacas;
-      vacas = await vacasService.getVacas(0,1000,null,null,userId,loteId);
-      if(vacas && vacas.length){
-        vacasId = vacas.map((val)=>{ return val.id;});
-      }else
-        return;
-    }
-    optionsFind.vaca = { $in: vacasId };
+  let vacas;
+  if (vacaId) {
+    vacas = await vacasService.getVacasById(vacaId, userId);
+    if (!vacas)
+      throw { message: 'Vaca does not exists with id ' + vacaId };
+    optionsFind._id = mongoose.Types.ObjectId(vacaId);
+  }else if (vacaReference) {
+    vacaId = await vacasModel
+      .findOne({ reference: vacaReference })
+      .select('name reference _id');
+    if (vacaId)
+      optionsFind._id = mongoose.Types.ObjectId(vacaId.id);
+    else
+      throw { message: 'Vaca does not exists with reference ' + vacaReference };
+  } else {
+    vacas = await vacasService.getVacas(0, 10000, null, null, userId, loteId);
+    if (vacas && vacas.length) {
+      vacasId = vacas.map((val) => { return val._id; });
+    } else
+      throw { message: 'There is no vacas for user ' + userId + ' in lote with id ' + loteId };
+    optionsFind._id = { $in: vacasId };
   }
-  optionsFind.sampleDate = {};
-  if(fromDate)
-    optionsFind.sampleDate.$gte = fromDate;
-  if(untilDate)
-    optionsFind.sampleDate.$lte = untilDate;
-  if(!optionsFind.sampleDate.$gte && !optionsFind.sampleDate.$lte)
-    delete optionsFind.sampleDate;
+  optionsFind.actividades = {};
+  optionsFind.actividades.sampleDate = {}
+  if (fromDate)
+    optionsFind.actividades.sampleDate.$gte = fromDate;
+  if (untilDate)
+    optionsFind.actividades.sampleDate.$lte = untilDate;
+  if (!optionsFind.actividades.sampleDate.$gte && !optionsFind.actividades.sampleDate.$lte)
+    delete optionsFind.actividades;
   let actividades;
-  if(orderBy)
+  /*if(orderBy)
     actividades = await actividadesModel.find(optionsFind)
       .populate(populate)
       .skip(skip).limit(limit).sort(orderBy).skip(skip).limit(limit);
   else
     actividades = await actividadesModel.find(optionsFind)
-    .populate(populate).skip(skip).limit(limit);
+    .populate(populate).skip(skip).limit(limit);*/
+  let aggregate = [
+    { $match: optionsFind },
+    {
+      $lookup: {
+        from: "actividades",
+        localField: "_id",
+        foreignField: "vaca",
+        as: "actividades"
+      }
+    }
+    , {
+      $lookup: {
+        from: "lotes",
+        localField: "location",
+        foreignField: "_id",
+        as: "location"
+      }
+    }
+    , { $unwind: "$location" }
+    , {
+      $project: {
+        actividades: {
+          /*          sampleDate: 1,
+                    latitude: 1,
+                    longitude: 1,*/
+          $slice: ["$actividades", skip, limit]
+        },
+        name: 1, sex: 1, reference: 1, months: 1, weight: 1,
+        'location.name': 1, 'location.latitude': 1,
+        'location.longitude': 1, 'location._id': 1
+      }
+    }
+  ];
+  if (orderBy)
+    aggregate.push({ $sort: { orderBy: 1 } });
+  actividades = await vacasModel.aggregate(aggregate);
   return actividades;
 }
--- src/service/VacasService.js
+++ src/service/VacasService.js
@@ -2,9 +2,12 @@
 
 const vacaModel = require('../database/models/vacas');
 const loteModel = require('../database/models/lotes');
+const actModel = require('../database/models/actividades');
 
-const populate = {path: 'location', select: 'name owner',populate: { path: 'owner', select: '_id user' }};
-const populate2 = {path: 'actividades', select: 'sampleDate latitude longitude _id'};
+const mongoose = require('../database/main');
+
+const _ = require('lodash');
+const populate = { path: 'location', select: 'name owner', populate: { path: 'owner', select: '_id user' } };
 /**
  * Add one vaca.
  * Add one vaca.
@@ -12,26 +15,26 @@ const populate2 = {path: 'actividades', select: 'sampleDate latitude longitude _
  * vaca Vacas
  * returns Vacas
  **/
-exports.addVaca = async function(vaca,userId) {
-  if(!vaca.location_id){
-    if(vaca.location && vaca.location._id)
+exports.addVaca = async function (vaca, userId) {
+  if (!vaca.location_id) {
+    if (vaca.location && vaca.location._id)
       vaca.location_id = vaca.location._id;
     else
-      throw { message: 'Location ID missing.'};
+      throw { message: 'Location ID missing.' };
   }
   let optionsLote = { _id: vaca.location_id };
-  if(userId){
+  if (userId) {
     optionsLote.owner = userId;
   }
   let lote = await loteModel.findOne(optionsLote);
-  if(!lote)
-    throw { message: 'Location ID does not exist (' + vaca.location_id +') or does not belongs to this user.'};
-  if(vaca.reference){
+  if (!lote)
+    throw { message: 'Location ID does not exist (' + vaca.location_id + ') or does not belongs to this user.' };
+  if (vaca.reference) {
     let vacaRef = await vacaModel.findOne({ reference: vaca.reference });
-    if(vacaRef)
+    if (vacaRef)
       throw { message: 'Reference already exists' };
-  }else{
-    throw { message: 'Reference missing.'};
+  } else {
+    throw { message: 'Reference missing.' };
   }
   let vacaDatabase = new vacaModel({
     name: vaca.name,
@@ -43,7 +46,7 @@ exports.addVaca = async function(vaca,userId) {
   });
   await vacaDatabase.save();
   vacaDatabase = vacaDatabase.toObject();
-  let savedVaca = await vacaModel.findOne({_id: vacaDatabase._id }).populate({path: 'location', select: 'name'});
+  let savedVaca = await vacaModel.findOne({ _id: vacaDatabase._id }).populate({ path: 'location', select: 'name' });
   savedVaca = savedVaca.toObject();
   delete savedVaca['__v'];
   return savedVaca;
@@ -57,17 +60,17 @@ exports.addVaca = async function(vaca,userId) {
  * id Long id to delete
  * returns DeletedResponse
  **/
-exports.deleteVaca = async function(id,userId) {
+exports.deleteVaca = async function (id, userId) {
   let options = { _id: id };
   let vaca = await vacaModel.findOne(options).populate(populate);
-  if(!vaca)
+  if (!vaca)
     throw { message: 'Vaca does not exists with ID: ' + id };
-  if(userId && vaca.location.owner.id !== userId)
+  if (userId && vaca.location.owner.id !== userId)
     throw { message: 'Vaca does not belong to user: ' + userId };
   vaca = await vacaModel.findByIdAndRemove(id);
-  if(!vaca)
+  if (!vaca)
     throw { message: 'ID does not exists' + id };
-  return {id: id};
+  return { id: id };
 }
 
 
@@ -81,31 +84,31 @@ exports.deleteVaca = async function(id,userId) {
  * lote Vacas
  * returns Vacas
  **/
-exports.editVaca = async function(vaca,userId) {
-  if(!vaca._id){
-    throw { message: '_id missing.'};
+exports.editVaca = async function (vaca, userId) {
+  if (!vaca._id) {
+    throw { message: '_id missing.' };
   }
-  if(!vaca.location_id){
-    if(vaca.location && vaca.location._id)
+  if (!vaca.location_id) {
+    if (vaca.location && vaca.location._id)
       vaca.location_id = vaca.location._id;
     else
-      throw { message: 'Location ID missing.'};
+      throw { message: 'Location ID missing.' };
   }
   let optionsLote = { _id: vaca.location_id };
-  if(userId)
+  if (userId)
     optionsLote.owner = userId;
   let lote = await loteModel.findOne(optionsLote);
-  if(!lote)
-    throw { message: 'Lote ID does not exist or does not belongs to user (' + vaca.location_id +').'};
-  if(userId){
+  if (!lote)
+    throw { message: 'Lote ID does not exist or does not belongs to user (' + vaca.location_id + ').' };
+  if (userId) {
     let vacacheck = await vacaModel.findById(vaca._id).populate(populate);
-    if(!vacacheck)
-      throw { message: 'Vaca with id does not exist.'};
-    if(vacacheck.location.owner.id !== userId)
-      throw { message: 'Vaca does not belongs to user.'};
+    if (!vacacheck)
+      throw { message: 'Vaca with id does not exist.' };
+    if (vacacheck.location.owner.id !== userId)
+      throw { message: 'Vaca does not belongs to user.' };
   }
   let vacaDatabase = new vacaModel({
-    _id : vaca._id,
+    _id: vaca._id,
     name: vaca.name,
     location: vaca.location_id,
     reference: vaca.reference,
@@ -113,10 +116,9 @@ exports.editVaca = async function(vaca,userId) {
     months: vaca.months,
     weight: vaca.weight
   });
-  await vacaModel.findOneAndUpdate({ _id: vaca._id },vacaDatabase);
+  await vacaModel.findOneAndUpdate({ _id: vaca._id }, vacaDatabase);
   let savedVaca = await vacaModel.findById(vaca._id)
-    .populate(populate)
-    .populate(populate2);
+    .populate(populate);
   delete savedVaca['__v'];
   return savedVaca;
 }
@@ -133,43 +135,70 @@ exports.editVaca = async function(vaca,userId) {
  * userId Long id of user. Only for admin users. (optional)
  * returns List
  **/
-exports.getVacas = async function(skip,limit,orderBy,filter,userId,loteId) {
-  if(limit <= 0)
+exports.getVacas = async function (skip, limit, orderBy, filter, userId, loteId) {
+  if (limit <= 0)
     limit = 10;
   let vaca;
   let find = {};
-  if(userId){
-    let lotesId = await loteModel.find({owner: userId}).select("_id");
-    if(!lotesId || !lotesId.length)
-      throw { message: 'User does have any lotes.'};
-    if(loteId){
-      if(lotesId.map(((lot) => {return lot.id;})).indexOf(loteId) === -1){
-        throw { message: 'User does have lote with id ' + loteId};
+  if (userId) {
+    let lotesId = await loteModel.find({ owner: userId }).select("_id");
+    if (!lotesId || !lotesId.length)
+      throw { message: 'User does have any lotes.' };
+    if (loteId) {
+      if (lotesId.map(((lot) => { return lot.id; })).indexOf(loteId) === -1) {
+        throw { message: 'User does have lote with id ' + loteId };
       }
-      find.location = loteId;
-    }else{
-      find.location = { $in: lotesId};
+      find.location = mongoose.Types.ObjectId(loteId);
+    } else {
+      find.location = { $in: lotesId.map(((lot) => { return mongoose.Types.ObjectId(lot.id); })) };
     }
-  }else{
-    if(loteId){
+  } else {
+    if (loteId) {
       let lote = await loteModel.findById(loteId);
-      if(!lote){
-        throw { message: 'Lote does not exist ' + loteId};
-      }else{
-        find.location = loteId;
+      if (!lote) {
+        throw { message: 'Lote does not exist ' + loteId };
+      } else {
+        find.location = mongoose.Types.ObjectId(loteId);
       }
     }
   }
-  if(orderBy)
-    vaca = await vacaModel.find(find)
-      .populate(populate)
-      .populate(populate2)
-      .skip(skip).limit(limit).sort(orderBy);
-  else
-    vaca = await vacaModel.find(find)
-    .populate(populate)
-    .populate(populate2)
-    .skip(skip).limit(limit);
+  let aggregate = [
+    { $match: find }
+    ,{
+      $lookup: {
+        from: "actividades",
+        localField: "_id",
+        foreignField: "vaca",
+        as: "actividades"
+      }
+    }
+    , {
+      $lookup: {
+        from: "lotes",
+        localField: "location",
+        foreignField: "_id",
+        as: "location"
+      }
+    }
+    , { $unwind: "$location" }
+    , { $skip: skip }, { $limit: limit }
+    ,{
+      $project: {
+        actividades: {
+/*          sampleDate: 1,
+          latitude: 1,
+          longitude: 1,*/
+          $slice: ["$actividades", -5]
+        },
+        name: 1, sex: 1, reference: 1, months: 1, weight: 1,
+        'location.name': 1, 'location.latitude': 1,
+        'location.longitude': 1, 'location._id': 1
+      }
+    }
+  ];
+  if (orderBy)
+    aggregate.push({ $sort: { orderBy: 1 } });
+  vaca = await vacaModel.aggregate(aggregate);
   return vaca;
 }
 
@@ -181,14 +210,14 @@ exports.getVacas = async function(skip,limit,orderBy,filter,userId,loteId) {
  * id Long id to delete
  * returns Vacas
  **/
-exports.getVacasById = async function(id,userId) {
+exports.getVacasById = async function (id, userId) {
   let options = { _id: id };
-  let vaca = await vacaModel.findOne(options).populate(populate)
-    .populate(populate2);
-  if(!vaca)
+  let vaca = await vacaModel.findOne(options).populate(populate);
+  if (!vaca)
     throw { message: 'Vaca does not exists with ID: ' + id };
-  if(userId && vaca.location.owner.id !== userId)
+  if (userId && vaca.location.owner.id !== userId)
     throw { message: 'Vaca does not belong to user: ' + userId };
+  vaca.actividades = await actModel.find({vaca:id}).limit(5).sort({sampleDate:-1});
   return vaca;
 }
 
